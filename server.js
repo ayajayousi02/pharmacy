4f694c0dffd53b8164034ddf76503644a570eec2
Aya Al-Jayousi <150697427+ayajayousi02@users.noreply.github.com>
2026-03-22 20:04:36 +0300

    server.js

--- server.js
+++ server.js
@@ -18,3 +18,5 @@ mongoose.connect(process.env.MONGO_URI)
     app.listen(5000, () => console.log("🚀 Server running on port 5000"));
   })
   .catch(err => console.error("❌ DB connection error:", err));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));

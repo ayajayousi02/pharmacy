c70735101a057561f92f107fe2dddb1e0564ff94
Aya Al-Jayousi <150697427+ayajayousi02@users.noreply.github.com>
2026-03-26 13:36:20 +0300

    server.js

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const authRoutes = require('./routes/authRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
-
+const productRoutes = require("./routes/productRoutes");
 const app = express();
 app.use(express.json());
-
+app.use("/uploads", express.static("uploads"));
 // Routes
 app.use('/auth', authRoutes);
 app.use('/categories', categoryRoutes);
-
+app.use("/api/products", productRoutes);
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {

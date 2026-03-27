e775fcf0388f767cecc2df33f17a20c751b58464
Aya Al-Jayousi <150697427+ayajayousi02@users.noreply.github.com>
2026-03-27 18:23:35 +0300

    Product.js

--- Product.js
+++ Product.js
@@ -1,26 +1,13 @@
-const mongoose = require("mongoose");
+
+    const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  price: {
-    type: Number,
-    required: true
-  },
-  description: {
-    type: String
-  },
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String },
   sideEffects: {
-    positive: {
-      type: String,
-      default: ""
-    },
-    negative: {
-      type: String,
-      default: ""
-    }
+    positive: { type: String, default: "" },
+    negative: { type: String, default: "" }
   },
   images: {
     type: [String], // مصفوفة روابط/مسارات للصور
@@ -30,12 +17,7 @@ const productSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "Category",
     required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
   }
-});
+}, { timestamps: true });
 
 module.exports = mongoose.model("Product", productSchema);
-

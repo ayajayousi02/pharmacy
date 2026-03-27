e990d5a3cffb147a375a461658eeafc7c05d0b38
Aya Al-Jayousi <150697427+ayajayousi02@users.noreply.github.com>
2026-03-27 18:25:46 +0300

    authMiddleware.js

--- authMiddleware.js
+++ authMiddleware.js
@@ -2,14 +2,20 @@ const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
+  
+  // لازم يكون التوكن موجود ويبدأ بـ Bearer
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "No token provided" });
   }
 
   const token = authHeader.split(" ")[1];
   try {
+    // التحقق من التوكن باستخدام الـ secret
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // فيه id, name, email
+    
+    // هون صار عندك كل المعلومات: id, name, email, role
+    req.user = decoded;
+    
     next();
   } catch (error) {
     res.status(401).json({ message: "Invalid or expired token" });

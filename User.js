d5e9e767699efc257a1fc625e3765fca7352872c
Aya Al-Jayousi <150697427+ayajayousi02@users.noreply.github.com>
2026-05-13 00:19:40 +0300

    Update User.js

--- User.js
+++ User.js
@@ -1,11 +1,37 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true }, 
-  email: { type: String, required: true, unique: true }, 
-  password: { type: String, required: true }, 
-  role: { type: String, enum: ['admin', 'user'], default: 'user' }, // أضفنا الرول
-  resetCode: { type: String } // نخزن الكود مؤقتًا
+  firstName: { 
+    type: String, 
+    required: true 
+  },
+
+  lastName: { 
+    type: String, 
+     default: ""
+  },
+
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+
+  password: { 
+    type: String, 
+    required: true 
+  },
+
+  role: { 
+    type: String, 
+    enum: ['admin', 'user'], 
+    default: 'user' 
+  },
+
+  resetCode: { 
+    type: String 
+  }
+
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);

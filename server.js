a2b56e4722d7edf923238aeb154daa945753e2ba
Aya Al-Jayousi <150697427+ayajayousi02@users.noreply.github.com>
2026-05-13 00:20:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const authRoutes = require('./routes/authRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
 const productRoutes = require("./routes/productRoutes");
 const userRoutes = require("./routes/userRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const couponRoutes = require("./routes/couponRoutes");
+const notificationRoutes = require("./routes/notificationRoutes");
+const prescriptionRoutes = require("./routes/prescriptionRoutes");
+const reportRoutes = require("./routes/reportRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+const wishlistRoutes = require("./routes/wishlistRoutes");
 // Middleware
 const authMiddleware = require('./middleware/authMiddleware');
 const adminMiddleware = require('./middleware/adminMiddleware');
@@ -26,6 +33,14 @@ app.use('/categories', categoryRoutes);
 // Product Routes
 app.use("/api/products", productRoutes);
 app.use("/users", userRoutes);
+app.use("/orders", orderRoutes);
+app.use("/coupons", couponRoutes);
+app.use("/prescriptions", prescriptionRoutes);
+app.use("/notifications", notificationRoutes);
+app.use("/reports", reportRoutes);
+app.use("/cart", cartRoutes);
+app.use("/wishlist", wishlistRoutes);
+
 // Admin Dashboard Route
 app.get('/admin/dashboard', authMiddleware, adminMiddleware, (req, res) => {
   res.json({ message: 'Welcome Admin!', user: req.user });
@@ -38,6 +53,10 @@ app.get('/redirect', authMiddleware, (req, res) => {
   } else {
     return res.json({ redirect: '/home' });
   }
+});
+app.get('/', (req, res) => {
+    console.log("Root route hit!");
+  res.send('Welcome to Pharmacy API 🚀');
 });
 
 // MongoDB Connection
